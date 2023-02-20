3ff0069f77784ecd1bcf844901be236d583896bc
chuong2512 <hieutg244@gmail.com>
2023-02-20 22:54:53 +0700

    update

--- Assets/_App/Scripts/CoinManager/PurchasingManager.cs
+++ Assets/_App/Scripts/CoinManager/PurchasingManager.cs
@@ -4,31 +4,35 @@ using UnityEngine;
 
 public class PurchasingManager : MonoBehaviour
 {
-   public void OnPressDown(int i)
-   {
-      switch (i)
-      {
-         case 1:
-            GameDataManager.Instance.playerData.AddDiamond(1);
-             IAPManager.Instance.BuyProductID(IAPKey.PACK1);
-            break;
-         case 2:
-            GameDataManager.Instance.playerData.AddDiamond(10);
-            IAPManager.Instance.BuyProductID(IAPKey.PACK2);
-            break;
-         case 3:
-            GameDataManager.Instance.playerData.AddDiamond(20);
-            IAPManager.Instance.BuyProductID(IAPKey.PACK3);
-            break;
-         case 4:
-            GameDataManager.Instance.playerData.AddDiamond(50);
-            IAPManager.Instance.BuyProductID(IAPKey.PACK4);
-            break;
-      }
-   }
+    public void OnPressDown(int i)
+    {
+        switch (i)
+        {
+            case 1:
+                IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(1); };
 
-   public void Sub(int i)
-   {
-      GameDataManager.Instance.playerData.SubDiamond(i);
-   }
+                IAPManager.Instance.BuyProductID(IAPKey.PACK1);
+                break;
+            case 2:
+                IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(10); };
+
+                IAPManager.Instance.BuyProductID(IAPKey.PACK2);
+                break;
+            case 3:
+                IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(20); };
+
+                IAPManager.Instance.BuyProductID(IAPKey.PACK3);
+                break;
+            case 4:
+                IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(50); };
+
+                IAPManager.Instance.BuyProductID(IAPKey.PACK4);
+                break;
+        }
+    }
+
+    public void Sub(int i)
+    {
+        GameDataManager.Instance.playerData.SubDiamond(i);
+    }
 }
